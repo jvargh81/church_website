a15b30adfb165525addf4535392f6c613caf6d0b
Jerrin Joe Varghese <jvargh81@lakers.mercyhurst.edu>
2019-04-07 19:04:45 -0400

    Updated login error checks.
    Signed-off-by: Jerrin Joe Varghese <jvargh81@lakers.mercyhurst.edu>

--- home.py
+++ home.py
@@ -37,10 +37,16 @@ def register():
     return render_template('register.html',title='Register',form=form)
 
 
-@app.route("/login")
+@app.route("/login",methods=['GET', 'POST'])
 
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data== 'password':
+            flash(f'Logged In to {form.email.data}!','success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Failed to login. please retry again', 'danger')
     return render_template('login.html',title='Login',form=form)
 
 
